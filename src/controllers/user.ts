893d828b3c93080a676102f72f54d7109390dbea
gebhartn <nicholas.gebhart@gmail.com>
2020-04-20 17:08:27 -0700

    feat: adds updatePassword user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -18,4 +18,33 @@ export const User = {
 
     res.status(201).json({ id, user, createdAt })
   },
+
+  updatePassword: async (req: Request, res: Response) => {
+    const { id } = res.locals.payload
+    const { oldPassword, newPassword } = req.body
+    if (!(oldPassword && newPassword)) {
+      res.status(400).send()
+    }
+
+    let user
+
+    try {
+      user = await prisma.user.findOne({ where: { id: Number(id) } })
+    } catch (id) {
+      res.status(401).send()
+    }
+
+    if (user && bcrypt.compareSync(oldPassword, user.password)) {
+      res.status(401).json({ err: 'Password must be different' })
+    }
+
+    const hashed = bcrypt.hashSync(newPassword, 8)
+
+    await prisma.user.update({
+      where: { id: Number(id) },
+      data: { password: hashed },
+    })
+
+    res.status(204).send()
+  },
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { User } from '../controllers/user'
 import { Auth } from '../controllers/auth'
+import checkJwt from '../utils/middlewares'
 
 const router = Router()
 
 router.post('/register', User.register)
+router.put('/update', [checkJwt], User.updatePassword)
 router.post('/login', Auth.login)
 
 export default router
