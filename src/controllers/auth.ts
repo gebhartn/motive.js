7025a30badb23f574dd18130e95bc5ef65ae0465
gebhartn <nicholas.gebhart@gmail.com>
2020-04-25 01:38:20 -0700

    chore: updates auth controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,37 +2,31 @@ import { PrismaClient, User } from '@prisma/client'
 import { Request, Response } from 'express'
 import * as bcrypt from 'bcryptjs'
 import signJwt from '../utils/signJwt'
+import { wrap } from '../utils/middlewares'
 
 const prisma = new PrismaClient()
 
 export const AuthController = {
   //! =================================================
   //! Login as a valid user
-  //!  Body:
-  //!    username: String (reqired): username
-  //!    password: String (reqired): password
   //! =================================================
-  login: async (req: Request, res: Response) => {
+  login: wrap(async (req: Request, res: Response) => {
     const { username, password } = req.body
 
-    if (!(username && password)) {
+    if (!(username && password))
       return res.status(400).json({ err: 'Must provide username and password' })
-    }
 
-    let user: User | null = null
+    let user: User | null
 
-    try {
-      user = await prisma.user.findOne({ where: { username } })
-    } catch (err) {
-      return res.status(401).json({ err: 'That user does not exist' })
-    }
+    user = await prisma.user.findOne({ where: { username } })
 
-    if (!(user && bcrypt.compareSync(password, user.password))) {
+    if (!user) return res.status(400).json({ err: 'Invalid username' })
+
+    if (!bcrypt.compareSync(password, user.password))
       return res.status(401).json({ err: 'Invalid password' })
-    }
 
     const token = user && signJwt(user.id)
 
     res.status(200).json({ payload: token })
-  },
+  }),
 }
